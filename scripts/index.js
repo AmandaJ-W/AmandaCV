a2326d9d23e1ff6689dae630547f30bb820fb558
Amanda Walker <amanda.j.2243@gmail.com>
2024-04-03 13:06:13 -0400

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+let skills = ['Java', 'JavaScript', 'HTML', 'CSS', 
+'SQL', 'Vue.js', 'Spring Boot', 'PostgreSQL', 'Web API', 'REST',
+'IntelliJ', 'VS Code', 'Git', 'Responsive Design', 'Unit Testing (JUnit)', 
+'E/R Diagrams', 'Integration Testing', 'Wordpress'];
+
+function displaySkills(skills) {
+    const skillsContainer = document.querySelector('.skills__item');
+
+    const skillsText = skills.join(', ');
+    // const skillsText = skills.map(skill => `• ${skill}`).join('\n');
+
+    const skillParagraph = document.createElement('p');
+    skillParagraph.textContent = skillsText;
+    
+    skillsContainer.appendChild(skillParagraph);
+}
+
+displaySkills(skills);
+
+const seeMoreButtons = document.querySelectorAll('.see-more');
+
+seeMoreButtons.forEach( button => {
+    button.addEventListener('click', function() {
+        const description = this.nextElementSibling;
+        description.classList.toggle('show');
+        this.textContent = description.classList.contains('+') ? 
+        '-' : '+';
+    });
+
+});
+
+
+
+
+});
