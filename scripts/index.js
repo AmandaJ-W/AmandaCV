2168317b65e3cbe361c044a098c8b238814582c7
Amanda Walker <amanda.j.2243@gmail.com>
2024-04-05 11:54:05 -0400

    formatting changes for small screens in the experience section

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,56 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  let skills = [
+    "Java",
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "SQL",
+    "Vue.js",
+    "Spring Boot",
+    "PostgreSQL",
+    "Web API",
+    "REST",
+    "IntelliJ",
+    "VS Code",
+    "Git",
+    "Responsive Design",
+    "Unit Testing (JUnit)",
+    "E/R Diagrams",
+    "Integration Testing",
+    "Wordpress",
+  ];
 
-let skills = ['Java', 'JavaScript', 'HTML', 'CSS', 
-'SQL', 'Vue.js', 'Spring Boot', 'PostgreSQL', 'Web API', 'REST',
-'IntelliJ', 'VS Code', 'Git', 'Responsive Design', 'Unit Testing (JUnit)', 
-'E/R Diagrams', 'Integration Testing', 'Wordpress'];
+  function displaySkills(skills) {
+    const skillsContainer = document.querySelector(".skills-item");
 
-function displaySkills(skills) {
-    const skillsContainer = document.querySelector('.skills-item');
-
-    const skillsText = skills.join(', ');
+    const skillsText = skills.join(", ");
     // const skillsText = skills.map(skill => `• ${skill}`).join('\n');
 
-    const skillParagraph = document.createElement('p');
+    const skillParagraph = document.createElement("p");
     skillParagraph.textContent = skillsText;
-    
+
     skillsContainer.appendChild(skillParagraph);
-}
+  }
 
-displaySkills(skills);
+  displaySkills(skills);
 
-const seeMoreButtons = document.querySelectorAll('.see-more');
+  const seeMoreButtons = document.querySelectorAll(".see-more");
 
-seeMoreButtons.forEach( button => {
-    button.addEventListener('click', function() {
-        const description = this.nextElementSibling;
-        description.classList.toggle('show');
-        this.textContent = description.classList.contains('+') ? 
-        '-' : '+';
+  seeMoreButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const description = this.nextElementSibling;
+      description.classList.toggle("show");
+      this.textContent = description.classList.contains("show") ? "-" : "+";
     });
+  });
 
-});
+  const seeDescriptionButtons = document.querySelectorAll(".show-desc");
 
-
-
-
+  seeDescriptionButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const duties = this.nextElementSibling;
+      duties.classList.toggle("show");
+      this.textContent = duties.classList.contains("show") ? "-" : "+";
+    });
+  });
 });
